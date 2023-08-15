27cb423f6c0a4c2b122fbba90d1306295ea683bb
houssemhassani <houssem.hassani@esprit.tn>
2023-08-15 13:04:42 +0200

    PayRollPdfCommit

---
+++ src/Service/PayRollService.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Service;
+use App\Repository\UserRepository;
+use App\Repository\AttendanceRecordRepository;
+use App\Repository\AttendanceRepository;
+use App\Repository\PayRollRepository;
+
+use App\Entity\User;
+use App\Entity\Attendance;
+use App\Entity\AttendanceRecord;
+
+
+ class PayRollService {
+    private UserRepository $userRepository ;
+    private AttendanceRepository $attendanceRepository;
+    private AttendanceRecordRepository $attendanceRecordRepository;
+
+    public function getEmploye(int $id){
+        return $this->userRepository->find($id);
+    }
+    public function getPayRollByEmployeId($employeeId,PayRollRepository $payRollRepository)
+    {
+        return $payRollRepository->findByEmployeeId($employeeId);
+    }
+ }
