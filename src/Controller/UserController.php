ddcb5f37e72ac1e5d3ffc4a64e1e3680e8056e37
houssemhassani <houssem.hassani@esprit.tn>
2023-08-17 12:49:39 +0200

    grhCrud

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -12,19 +12,26 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/employe')]
+#[Route('/user')]
 class UserController extends AbstractController
 {
-    #[Route('/', name: 'app_employe_index', methods: ['GET'])]
+    #[Route('/employe', name: 'app_employe_index', methods: ['GET'])]
     public function index(UserRepository $userRepository): Response
     {
         return $this->render('employe/index.html.twig', [
             'users' => $userRepository->findAll(),
         ]);
     }
+    #[Route('/grh', name: 'app_grh_index', methods: ['GET'])]
+    public function indexGrh(UserRepository $userRepository): Response
+    {
+        return $this->render('grh/index.html.twig', [
+            'users' => $userRepository->findAll(),
+        ]);
+    }
 
-    #[Route('/new', name: 'app_employe_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    #[Route('/newEmploye', name: 'app_employe_new', methods: ['GET', 'POST'])]
+    public function newEmploye(Request $request, EntityManagerInterface $entityManager): Response
     {
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
@@ -42,18 +49,46 @@ class UserController extends AbstractController
             'form' => $form,
         ]);
     }
-
-    #[Route('/{id}', name: 'app_employe_show', methods: ['GET'])]
-    public function show(User $user,PdfService $pdf): Response
+    #[Route('/newGrh', name: 'app_grh_new', methods: ['GET', 'POST'])]
+    public function newGRH(Request $request, EntityManagerInterface $entityManager): Response
     {
-        $html=$this->render('employe/show.html.twig',['user'=>$user]);
-        $pdf->showPdFile($html);
-        /* return $this->render('employe/show.html.twig', [
+        $user = new User();
+        $user->setRoles(["GRH"]);
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_grh_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('grh/new.html.twig', [
             'user' => $user,
-        ]); */
+            'form' => $form,
+        ]);
+    }
+    #[Route('/employe/{id}', name: 'app_employe_show', methods: ['GET'])]
+    public function show(User $user,PdfService $pdf)
+    {
+        /*$html = $this->render('employe/show.html.twig', ['user' => $user]);
+        $pdf->showPdFile($html);*/
+         return $this->render('employe/show.html.twig', [
+            'user' => $user,
+        ]);
+    }
+    #[Route('/grh/{id}', name: 'app_grh_show', methods: ['GET'])]
+    public function showGrh(User $user,PdfService $pdf)
+    {
+        /*$html = $this->render('employe/show.html.twig', ['user' => $user]);
+        $pdf->showPdFile($html);*/
+        return $this->render('grh/show.html.twig', [
+            'user' => $user,
+        ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_employe_edit', methods: ['GET', 'POST'])]
+    #[Route('/{id}/edit/employe', name: 'app_employe_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(UserType::class, $user);
@@ -70,9 +105,35 @@ class UserController extends AbstractController
             'form' => $form,
         ]);
     }
+    #[Route('/{id}/edit/Grh', name: 'app_grh_edit', methods: ['GET', 'POST'])]
+    public function editGrh(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
 
-    #[Route('/{id}', name: 'app_employe_delete', methods: ['POST'])]
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_grh_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('grh/edit.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+    #[Route('/{id}/employe', name: 'app_employe_delete', methods: ['POST'])]
     public function delete(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($user);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_employe_index', [], Response::HTTP_SEE_OTHER);
+    }
+    #[Route('/{id}/grh', name: 'app_grh_delete', methods: ['POST'])]
+    public function deleteGrh(Request $request, User $user, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
             $entityManager->remove($user);
