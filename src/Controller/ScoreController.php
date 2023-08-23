c79cbe1f4d667eb7c94f9a0a10d11626076ab16f
houssemhassani <houssem.hassani@esprit.tn>
2023-08-23 15:50:10 +0200

    evaluationEmployee3

--- src/Controller/ScoreController.php
+++ src/Controller/ScoreController.php
@@ -7,6 +7,7 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\ScoreType;
 use App\Entity\Score;
+use App\Repository\ScoreRepository;
 use App\Service\EvaluationService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -51,6 +52,65 @@ class ScoreController extends AbstractController
         return $this->render('score/new.html.twig', [
             'form' => $form->createView(),
             'employee' => $employee,
+        ]);
+    }
+    #[Route('/employee/{employee}/scores', name: 'user_scores')]
+    public function getUserScores(User $employee ): Response
+    {
+        // Retrieve the user by ID
+        ;
+
+        if (!$employee) {
+            throw $this->createNotFoundException('User not found');
+        }
+
+        // Get the scores for the user
+        $scores = $this->evaluationService->getScoresByUserId($employee->getId() );
+
+        return $this->render('score/index.html.twig', [
+            'employee' => $employee,
+            'scores' => $scores,
+        ]);
+    }
+    #[Route('/statistics/scores/{userId}', name: 'user_scores_statistics')]
+    public function userScoresStatistics(int $userId, ScoreRepository $scoreRepository): Response
+    {
+        $user = $this->getDoctrine()->getRepository(User::class)->find($userId);
+
+        if (!$user) {
+            throw $this->createNotFoundException('User not found');
+        }
+
+        // Get scores for the user
+        $scores = $this->evaluationService->getScoresByUserId($userId);
+
+        // Calculate statistics
+        $statistics = [];
+        foreach ($scores as $score) {
+            $createdAt = $score->getCreatedAt();
+            $monthYear = $createdAt->format('Y-m');
+
+            if (!isset($statistics[$monthYear])) {
+                $statistics[$monthYear] = [
+                    'totalScores' => 0,
+                    'averageScore' => 0,
+                ];
+            }
+
+            $statistics[$monthYear]['totalScores']++;
+            $statistics[$monthYear]['averageScore'] += $score->getScoreEvaluation();
+        }
+
+        // Calculate average scores
+        foreach ($statistics as &$stat) {
+            if ($stat['totalScores'] > 0) {
+                $stat['averageScore'] /= $stat['totalScores'];
+            }
+        }
+
+        return $this->render('score/user_scores_statistics.html.twig', [
+            'user' => $user,
+            'statistics' => $statistics,
         ]);
     }
 }
--- src/Service/EvaluationService.php
+++ src/Service/EvaluationService.php
@@ -3,15 +3,18 @@ namespace App\Service;
 
 use App\Entity\Score;
 use App\Entity\User;
+use App\Repository\ScoreRepository;
 use Doctrine\ORM\EntityManagerInterface;
 
  class EvaluationService
 {
     private $entityManager;
+    private $scoreRepository;
 
-    public function __construct(EntityManagerInterface $entityManager)
+    public function __construct(ScoreRepository $scoreRepository,EntityManagerInterface $entityManager)
     {
         $this->entityManager = $entityManager;
+        $this->scoreRepository=$scoreRepository;
     }
 
     public function evaluateEmployee(User $employee, Score $score): void
@@ -21,4 +24,19 @@ use Doctrine\ORM\EntityManagerInterface;
         $this->entityManager->persist($score);
         $this->entityManager->flush();
     }
+     /**
+      * Get scores for a specific user by their ID.
+      *
+      * @param int $userId
+      * @return Score[]
+      */
+     public function getScoresByUserId(int $userId): array
+     {
+         return $this->scoreRepository->createQueryBuilder('s')
+             ->andWhere('s.employe = :userId')
+             ->setParameter('userId', $userId)
+             ->orderBy('s.created_At', 'DESC')
+             ->getQuery()
+             ->getResult();
+     }
 }
